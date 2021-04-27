b44856d16a2587e5ed3a7a0888dc19f37927f5a0
Shamil <shamillorsan@gmail.com>
2021-04-27 22:36:53 +0200

    new changes

---
+++ routes/frontend.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    console.log('GET /frontend');
+    //res.sendFile(__dirname + '/..frontend/index.html') //__dirname 
+    res.send('Frontend!')
+    })
+
+router.get('/style', (req, res) => {
+    console.log('GET /frontend/style')
+    res.send('font-weight: bold;')
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express') //Внедряем пакет express
 const app = express() //"app" должен по идее называться "server".
 
+const frontend = require('./routes/frontend.js') //Забираем модуль frontend
+
 const PORT = 1337
 
 
 // Ниже РОУТЫ
 //упраляет ресурсом "web root" - request и response
-app.get('/', (req, res)=> { //регистрирует роут на сервере
-console.log('GET /'); 
-res.send('Yes i am here')  //res.send - отправляет сообщение на страницу 
-})
+// app.get('/', (req, res)=> { //регистрирует роут на сервере
+// console.log('GET /'); 
+// res.send('Yes i am here')  //res.send - отправляет сообщение на страницу 
+// })
 
-app.get('/secret', (req, res) => {
-console.log('GET /secret');
-res.send('You have found secret route')
-})
+// app.get('/secret', (req, res) => {
+// console.log('GET /secret');
+// res.send('You have found secret route')
+// })
 
-app.get('/Frontend', (req, res) => {
-    console.log('GET /Frontend');
-    res.sendFile(__dirname + '/frontend/index.html') //__dirname 
-    })
+app.use('/frontend', frontend)
+
+
+
+
 
 
 //Стартует сервер
@@ -27,4 +30,3 @@ app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
 })
 
-console.log('This file is done')
