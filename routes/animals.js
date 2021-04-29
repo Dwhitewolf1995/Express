d001b2d50ae754c607f1ca2944bf96483aa7a434
Shamil <shamillorsan@gmail.com>
2021-04-29 10:40:32 +0200

    new changes for 29.04.21

--- routes/animals.js
+++ routes/animals.js
@@ -3,31 +3,61 @@ const { route } = require('./frontend');
 const router = express.Router()
 
 
+//=======================================================================================//
+//Обьект с фейковой базой данных
+
 
 //Animal info - a RESTful API
 //Animal object: { species, favoritefood}
+
+
+
 let fakeDb = [
-    {species: 'monkey', favoriteFood: 'banana'},
+    {species: 'monkey',favoriteFood: 'banana'},
     {species: 'cat', favoriteFood: 'mouse' },
     {species: 'tapir', favoriteFood: 'leaves'}
 ]
 
+
+//=======================================================================================//
 //GET animals
+
+
 router.get('/', (req, res) => {
-    console.log('GET /animals/');
+    //console.log('GET /animals/');
     res.send(fakeDb)
 
 })
 
+//=======================================================================================//
+//GET specifik animal
 
+router.get('/:index/', (req, res) => {
+    //console.log('GET /animals/1', req.params.index);
+    const index = Number(req.params.index)
+    res.send(fakeDb[index])
+    
+
+})
+
+
+
+//=======================================================================================//
 //POST animals
+
+
 router.post('/', (req, res) => {
-    console.log('POST /animals/', req.body);
+    //console.log('POST /animals/', req.body);
+
+
+
     // I vanliga fall får vi datan från klienten - i req objektet
 //let newAnimal = { species: 'kea', favoriteFood: 'rabbits' }
 
+
+
 // På riktigt 
-let newAnimal = req.body 
+let newAnimal = req.body+ 
 
 fakeDb.push(newAnimal)
 res.send('Added new animal')
@@ -35,9 +65,13 @@ res.send('Added new animal')
 })
 
 
+
+//=======================================================================================//
 // PUT animals
+
+
 router.put('/',(req, res) => {
-    console.log('PUT /animals');
+    //console.log('PUT /animals');
     let replaceAnimal = { species: 'seagull', favoriteFood: 'fish'}
     let lastIndex = fakeDb.length - 1
     fakeDb[lastIndex] = replaceAnimal
@@ -46,14 +80,17 @@ router.put('/',(req, res) => {
 })
 
 
+//=======================================================================================//
 // DELETE animals
 
 router.delete('/', (req, res) => {
-    console.log('Delete /animals');
+    //console.log('Delete /animals');
     fakeDb = []
     res.send('Deleted all animals')
 })
 
 
+
+//=======================================================================================//
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express') //Внедряем пакет express
+const path = require('path') 
 const app = express() //"app" должен по идее называться "server".
+
+
+
 
 const frontend = require('./routes/frontend.js') //Забираем модуль frontend
 const onGetGuestbook = require('./routes/guestbook.js')
 const animals = require('./routes/animals.js')
 
-const PORT = 1337
+const PORT = 1337 // Port server
+const staticFolder = path.join(__dirname, 'frontend')//static mapp
+
+
+
+//=======================================================================================//
 
 
 
@@ -15,25 +24,55 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
+//Собственная функция middleware
+app.use((req, res, next) => {
+    //Logger 
+    console.log(`${req.method} ${req.url}`, req.params);
+    next()
+
+})
+
+//=======================================================================================//
+//Static mapp
 
 
+app.use(express.static(staticFolder))
+
+
+
+
+
+//=======================================================================================//
+//Firestore project
+
+
+app.get('/', (req, res) => {
+    res.send('Firestore project')
+})
+
+
+
+
+//=======================================================================================//
 //Endpoints / resurser / routes
 
 
 
 
-// Ниже РОУТЫ
+
 //упраляет ресурсом "web root" - request и response
+
 app.get('/', (req, res)=> { //регистрирует роут на сервере
-console.log('GET /'); 
+//console.log('GET /'); 
 res.send('Yes i am here')  //res.send - отправляет сообщение на страницу 
 })
 
 app.get('/secret', (req, res) => {
-console.log('GET /secret');
+//console.log('GET /secret');
 res.send('You have found secret route')
 })
 
+//=======================================================================================//
 //Роуты
 
 app.get('/guestbook', onGetGuestbook )
@@ -47,8 +86,10 @@ app.use('/animals', animals)
 
 
 
-
+//=======================================================================================//
 //Стартует сервер
+
+
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
 })
